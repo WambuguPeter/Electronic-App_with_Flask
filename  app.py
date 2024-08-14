7ee1e0f7aa15abc1a32fa6948bdaa7ff0d1f0c30
TillHappens <gitiropeter4@gmail.com>
2024-08-14 07:44:59 +0300

    first commit

---  app.py
+++  app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session # type: ignore
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
